a56fdeed418a772ad48e95b645a4c564b3edb33d
jiwon9412 <80965224+jiwon9412@users.noreply.github.com>
2022-10-02 16:15:34 +0900

    gitignore 적용하기

--- src/reducers/index.jsx
+++
@@ -1,7 +0,0 @@
-import { combineReducers } from 'redux';
-import mode from './mode';
-import player from './player';
-
-const rootReducer = combineReducers({ mode, player });
-
-export default rootReducer;
